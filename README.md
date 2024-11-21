9c88311dc1888561e487f45f5bd3ceebd6ff19c3
trieuconcrete <trieunb@concrete-corp.com>
2024-11-21 15:38:49 +0700

    update setting database postgresql

--- README.md
+++ README.md
@@ -3,5 +3,7 @@
 ## Setup
 
 ### Create SECRET_KEY
+```
 python -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())'
 openssl rand -base64 32
+```
--- src/core/settings.py
+++ src/core/settings.py
@@ -94,6 +94,39 @@ DATABASES = {
     }
 }
 
+CONN_MAX_AGE = config("CONN_MAX_AGE", cast=str, default=30)
+DATABASE_URL = config("DATABASE_URL", cast=str)
+
+if DATABASE_URL is not None:
+    import dj_database_url
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=30,
+            conn_health_checks=True
+        )
+    }
+
+# Add these at the top of your settings.py
+# from os import getenv
+# from dotenv import load_dotenv
+
+# Replace the DATABASES section of your settings.py with this
+# tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
+
+# DATABASES = {
+#   'default': {
+#     'ENGINE': 'django.db.backends.postgresql',
+#     'NAME': config('PGDATABASE'),
+#     'USER': config('PGUSER'),
+#     'PASSWORD': config('PGPASSWORD'),
+#     'HOST': config('PGHOST'),
+#     'PORT': config('PGPORT', 5432),
+#     'OPTIONS': {
+#       'sslmode': 'require',
+#     },
+#   }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
