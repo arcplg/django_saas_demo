297844c531653bb45c8795953e19f12c74127874
trieuconcrete <trieunb@concrete-corp.com>
2024-11-22 09:49:05 +0700

    prepare static file

--- README.md
+++ README.md
@@ -7,3 +7,9 @@
 python -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())'
 openssl rand -base64 32
 ```
+
+
+### generate assets static file
+```
+python manage.py collectstatic
+```
--- src/core/settings.py
+++ src/core/settings.py
@@ -163,6 +163,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_BASE_DIRS = BASE_DIR / "staticfiles"
+STATICFILES_ASSETS_DIR = STATICFILES_BASE_DIRS / "assets"
+
+# source for python manage.py collectstatic
+
+STATICFILES_DIRS = [
+    STATICFILES_BASE_DIRS
+]
+
+# output for python manage.py collectstatic
+# local cdn
+# STATIC_ROOT = BASE_DIR.parent / "local-cdn"
+STATIC_ROOT = BASE_DIR / "local-cdn" # generate in src dir
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
