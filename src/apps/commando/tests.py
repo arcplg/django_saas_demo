5826b637d2f2b6329e1b1946bf2c17ba8b892cdf
trieuconcrete <trieunb@concrete-corp.com>
2024-12-21 15:32:01 +0700

    3 - Django Test ENV Vars

--- src/apps/commando/tests.py
+++ src/apps/commando/tests.py
@@ -3,7 +3,6 @@ from django.test import TestCase
 
 # Create your tests here.
 class NeonDBTestCase(TestCase):
-
     def test_db_url(self):
         DATABASE_URL = settings.DATABASE_URL
         self.assertIn("neon.tech", DATABASE_URL)
